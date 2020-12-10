59d2a02f0003b571e5d5833cb6c68de76abb49c8
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-12-10 20:39:42 +0100

    Fix accessibility

--- packages/zedocs/src/build/document/addHeadingLinks.ts
+++ packages/zedocs/src/build/document/addHeadingLinks.ts
@@ -2,7 +2,7 @@ export function addHeadingLinks($: cheerio.Root) {
   $('h1, h2, h3, h4, h5, h6').each((i, el) => {
     const $el = $(el)
     const id = $el.attr('id')
-    $el.append(` <a class="heading-link" href="#${id}">#</a>`)
+    $el.append(` <a class="heading-link" tabIndex="-1" href="#${id}">#</a>`)
     $el.html(`<div>${$el.html()}</div>`)
   })
 }
