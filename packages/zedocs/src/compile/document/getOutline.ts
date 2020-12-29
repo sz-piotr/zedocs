a87fe743f021d777a4f89f52173e844e88a0f054
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-12-29 18:40:45 +0100

    Fix types

--- packages/zedocs/src/compile/document/getOutline.ts
+++ packages/zedocs/src/compile/document/getOutline.ts
@@ -7,15 +7,17 @@ export interface OutlineItem {
 export function getOutline($: cheerio.Root) {
   const items: OutlineItem[] = []
   $('h1, h2, h3, h4, h5, h6').each((i, el) => {
-    const $el = $(el)
-    const level = parseInt(el.tagName[1])
-    const name = $el.text()
-    const id = $el.attr('id')
-    items.push({
-      name,
-      level,
-      link: id ? `#${id}` : undefined,
-    })
+    if (el.type === 'tag') {
+      const $el = $(el)
+      const level = parseInt(el.tagName[1])
+      const name = $el.text()
+      const id = $el.attr('id')
+      items.push({
+        name,
+        level,
+        link: id ? `#${id}` : undefined,
+      })
+    }
   })
   return items
 }
